4f98469a947da2c2c32fb6b34f968fb72af54e01
Brad King <bking5194@gmail.com>
2026-04-18 14:45:47 -0400

    odds changes a.0.3

--- I3P/game.js
+++ I3P/game.js
@@ -14,15 +14,15 @@ var pair_plus = 0;
 var hand_counter = 0;
 var stack_snapshot = stack;
 
-const to_qualify = 10; // King
+const to_qualify = 10; // 11 = King
 
 const pair_pay = 2;
 const flush_pay = 4;
 const straight_pay = 5;
 const flush_pair_pay = 6;
-const trips_pay = 10;
-const straight_flush_pay = 13;
-const flush_trips_pay = 25;
+const trips_pay = 11;
+const straight_flush_pay = 16;
+const flush_trips_pay = 21;
 
 //
 //Helper Functions
